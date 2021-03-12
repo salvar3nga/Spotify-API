93df59fdbea291b2e14f7f4edd524f25ca742007
sa <alvarengasergio@hotmail.co.uk>
2021-03-12 17:11:17 +0100

    modified the code

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const header = document.querySelector("#tableHeader");
-const stick = header.offsetTop;
 const btnConfirm = document.querySelector("#confirm");
-
-let nrInpt = document.querySelector("#number");
-let coverInpt = document.querySelector("#cover")
-let titleInpt = document.querySelector("#title")
-let streamsInpt = document.querySelector("#streams")
-let interpreterInpt = document.querySelector("#interpreter")
-let table = document.querySelector("tbody");
-
+const nrInpt = document.querySelector("#number");
+const coverInpt = document.querySelector("#cover")
+const titleInpt = document.querySelector("#title")
+const streamsInpt = document.querySelector("#streams")
+const interpreterInpt = document.querySelector("#interpreter")
+const tableBody = document.querySelector("tbody");
 
 
-window.onscroll = function (){
-    fixHeader();
-};
 
+/*
+    The following section is just to test the dynamically
+    adding of rows into the table with the click of the "ok" button
+    and some temporary input fields to emulate Data retrieval from the
+    Spotify API
 
-function fixHeader() {
-    window.pageYOffset > stick ? header.classList.add("fixedPosition") : header.classList.remove("fixedPosition")
-}
+    Also it gives me the chance to see if the table header will stay fixed 
+    to the top, upon insertion of multiple rows.
+
+    PS: The section will be deleted after testing 
+*/
+
 
 
 btnConfirm.addEventListener("click", ()=>{
@@ -28,16 +29,79 @@ btnConfirm.addEventListener("click", ()=>{
     let stream = streamsInpt.value;
     let int = interpreterInpt.value;
 
-    let template = `
+    const template = `
                 <tr>
                     <td>${nr}</td>
                     <td><img src="${cover}"></td>
                     <td>${title}</td>
                     <td>${stream}</td>
                     <td>${int}</td>
-                    <td><button>Details</button></td>
+                    <td><button class="details">Details</button></td>
                 </tr> `;
 
-        table.innerHTML += template;       
+    tableBody.innerHTML += template;       
 });
+
+
+
+
+/* 
+    Attempt at retrieving the Bearer token from Spotify!
+
+    In order to do this step you will need to provide the 
+    client ID & Client Secret you received from Spotify when 
+    creating your App under the developer Account.
+    We will later on, encode it and pass it to the header. 
+*/
+
+const getAccessToken = async () =>{
+    
+    //!!!!! Require Client ID and client Secret !!!!!!!
+    const clientID = " ";
+    const clientSecret = " ";
+
+   
+    try{
+        const res = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            mode: "no-cors",
+            headers: {
+                "Content_Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " +btoa(clientID+ ":" +clientSecret)
+                 
+            },
+            body: "grant_type=client_credentials"
+    })
+    }catch(err){
+      console.log("Cannot retreive Token", err);  
+    }
+
+    const data = await res.json();
+
+    //Logging the json data into the console in order to destructure it
+    //console.log(data);
+
+    return data.access_token;
+    
+}
+
+
+/*
+    Using the retreived token to access a Playlist / Chart
+    More on: https://developer.spotify.com/console/playlists/
+*/
+
+const getPlaylist = (token) => {
+    try{
+        //implement fetching the playlist
+    }catch(err){
+        console.log("Failed to get Playlist", err);
+    }
+}
+
+
+// Loop Data from the Playlist and display it on the table
+
+
+
 
